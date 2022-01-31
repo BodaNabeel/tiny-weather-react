867edf425086bf6aafdd56b2f4ba7e75d6d4fca8
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-01 00:21:57 +0530

    shifted fetching API from app.js to inputfield.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import WebFontLoader from "./components/hooks/WebFontLoader";
 import Header from "./components/Header";
 import InputField from "./components/InputField";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [location, setLocation] = useState("");
   const [weather, setWeather] = useState("");
 
-  const fetchAPI = (location) => {
-    const API_KEY = `ab00324bf4bc45a786780155223101`;
-    const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
+  // const fetchAPI = (location) => {
+  //   const API_KEY = `ab00324bf4bc45a786780155223101`;
+  //   const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setWeather(data));
-  };
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // };
 
   return (
     <>
@@ -23,8 +23,10 @@ function App() {
       <InputField
         setLocation={setLocation}
         location={location}
-        fetchAPI={fetchAPI}
+        // fetchAPI={fetchAPI}
+        setWeather={setWeather}
       />
+      <p>{weather.location? weather.location.name : "error"}</p>
     </>
   );
 }
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,13 +1,34 @@
 import React, { useEffect, useRef } from "react";
 
-function InputField({ location,setLocation, fetchAPI }) {
+function InputField({ location, setLocation, setWeather }) {
+  useEffect(() => {
+    if (location !== "") {
+      const API_KEY = `ab00324bf4bc45a786780155223101`;
+      const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => setWeather(data));
+    }
+  },[location]);
+
+  // const fetchAPI = (location) => {
+  // const API_KEY = `ab00324bf4bc45a786780155223101`;
+  // const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
+
+  // fetch(url)
+  //   .then((res) => res.json())
+  // .then((data) => console.log(data));
+  // };
+
   const input = useRef();
   useEffect(() => {
     input.current.addEventListener("keyup", (event) => {
       if (event.keyCode === 13 && input.current.value !== "") {
-        fetchAPI(location);
         setLocation(input.current.value);
         input.current.blur();
+
+        // fetchAPI(location);
       }
     });
   });
@@ -18,7 +39,7 @@ function InputField({ location,setLocation, fetchAPI }) {
       className="input-field"
       type="text"
       maxLength="18"
-      onChange={(e) => setLocation(e.target.value)}
+      // onChange={(e) => setLocation(e.target.value)}
       placeholder="enter place..."
     ></input>
   );

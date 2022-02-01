3c12d10db6432b92d4edfc2f397d85c57cbeb9bd
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-01 14:02:45 +0530

    styling added to weathercontainer

--- src/components/Container/WeatherContainer.jsx
+++ src/components/Container/WeatherContainer.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
+import React from "react";
 
-function WeatherContainer({weather}) {
-  const locatTime = weather.location.localtime.split(" ")
-  console.log(locatTime[1])
+function WeatherContainer({ weather }) {
+  const locatTime = weather.location.localtime.split(" ");
   return (
     <>
-    <div className="weather-top">
-      <p className='weather-title'>{`${weather.current.feelslike_c}°C | ${weather.current.condition.text}`}</p>
-      <img src={weather.current.condition.icon} alt="" className="weather-icon" />
-    </div>
-    <h2 className="weather-txt">{weather.location.name}, {weather.location.country}</h2>
-    <div className="weather-info">
+      <div className="weather-top">
+        <p className="weather-title">{`${weather.current.feelslike_c}°C`} 
+        <span className="weather-description">{` | ${weather.current.condition.text}`}</span>
+        </p>
+        <img
+          src={weather.current.condition.icon}
+          alt=""
+          className="weather-icon"
+        />
+      </div>
+      <h2 className="weather-txt">
+        {weather.location.name}, {weather.location.country}
+      </h2>
       <p className="weather-info__title">Weather Info</p>
-      <div className="weather-info__container">
-        <img src={weather.current.condition.icon} alt="" className="icon" />
-        <div className="content">
-          <p className="content-detail">{weather.current.humidity}</p>
-          <p className="content-title">Humidity</p>
+      <div className="weather-info">
+        <div className="weather-info__container">
+          <img src={weather.current.condition.icon} alt="" className="icon" />
+          <div className="content">
+            <p className="content-detail">{weather.current.humidity} <span className="content-title">Humidity</span></p>
+           
+          </div>
+        </div>
+        <div className="weather-info__container">
+          <img src={weather.current.condition.icon} alt="" className="icon" />
+          <div className="content">
+            <p className="content-detail">{weather.current.wind_kph} <span className="content-title">Wind</span></p>
+        
+          </div>
+        </div>
+        <div className="weather-info__container">
+          <img src={weather.current.condition.icon} alt="" className="icon" />
+          <div className="content">
+            <p className="content-detail">
+              {weather.current.pressure_in}
+              <span className="content-title">Pressure</span>
+            </p>
+          </div>
+        </div>
+        <div className="weather-info__container">
+          <img src={weather.current.condition.icon} alt="" className="icon" />
+          <div className="content">
+            <p className="content-detail">{locatTime[1]} <span className="content-title">Time</span></p>
+         
+          </div>
         </div>
       </div>
-      <div className="weather-info__container">
-        <img src={weather.current.condition.icon} alt="" className="icon" />
-        <div className="content">
-          <p className="content-detail">{weather.current.wind_kph}</p>
-          <p className="content-title">Wind</p>
-        </div>
-      </div>
-      <div className="weather-info__container">
-        <img src={weather.current.condition.icon} alt="" className="icon" />
-        <div className="content">
-          <p className="content-detail">{weather.current.pressure_in}</p>
-          <p className="content-title">Pressure</p>
-        </div>
-      </div>
-      <div className="weather-info__container">
-        <img src={weather.current.condition.icon} alt="" className="icon" />
-        <div className="content">
-          <p className="content-detail">{locatTime[1]}</p>
-          <p className="content-title">Time</p>
-        </div>
-      </div>
-    </div>
     </>
   );
 }
